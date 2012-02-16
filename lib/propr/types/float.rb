dbdbba76bb4abef524b1be6be7b1b822aa0f331b
kputnam <putnam.kyle@gmail.com>
2012-02-16 03:56:30 -0600

    Implement a few more instances

--- lib/propr/types/float.rb
+++ lib/propr/types/float.rb
@@ -1,22 +1,5 @@
 require "bigdecimal"
 
-class << Float
-  FLOATMAX = 1.7976931348623157e+308
-  FLOATMIN = -1.7976931348623157e+308
-
-  # @return [Float]
-  def propr(options = {})
-    # @todo: -Infinity, +Infinity, +0.0, -0.0, NaN
-    min = options.fetch(:min, FLOATMIN)
-    max = options.fetch(:max, FLOATMAX)
-
-    # Careful here to prevent Float overflow
-    int = rand(max.to_i - min.to_i + 1)
-    big = BigDecimal(int.to_s) + BigDecimal(min.to_s)
-    big.to_f + rand
-  end
-end
-
 class Float
   # @return [Enumerator<Float>]
   def propq
@@ -24,5 +7,29 @@ class Float
       seed_ = seed / 2
       (seed != seed_).maybe([self - seed, seed_])
     end
+  end
+end
+
+class << Float
+  # @return [Float]
+  def propr(options = {})
+    min = options.fetch(:min, -Float::INFINITY).to_f
+    max = options.fetch(:max,  Float::INFINITY).to_f
+
+    min_ = if min.finite? then min else -Float::MAX end
+    max_ = if max.finite? then max else  Float::MAX end
+
+    # Taking care to prevent Float overflow
+    int  = rand(max_.to_i - min_.to_i + 1)
+    big  = BigDecimal(int.to_s) + BigDecimal(min_.to_s)
+    big += rand
+
+    # @todo: This is not a useful distribution
+    #hoices = [big]
+    #hoices.push(-Float::INFINITY)  unless max.finite?
+    #hoices.push(Float::INFINITY)   unless max.finite?
+    #hoices.push(-0.0)       if min <= 0 and max >= 0
+    #hoices.push(Float::NAN) if options.fetch(:nan, true)
+    #hoices.propo
   end
 end
