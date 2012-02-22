e5613c6f99df384f7482077d30daceab21158861
kputnam <235798+kputnam@users.noreply.github.com>
2012-02-22 07:45:35 -0600

    Define #shrink on Array and String

--- lib/propr/types/array.rb
+++ lib/propr/types/array.rb
@@ -8,10 +8,13 @@ class Array
   end
 
   def shrink
-    return [] if empty?
-
-    Enumerator.new do |yielder|
+    combination(size - 1).to_a.tap do |shrunken|
+      shrunken << []
       size.times do |n|
+        head = self[0, n]
+        tail = self[n+1..-1]
+        item = self[n]
+        shrunken.concat(item.shrink.map{|m| head + [m] + tail })
       end
     end
   end
