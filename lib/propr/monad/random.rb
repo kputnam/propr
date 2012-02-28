e6dffdca33aef9040b122cff9778d52f97d63422
kputnam <putnam.kyle@gmail.com>
2012-02-28 17:27:50 -0600

    Implement Random.guard (ick) and BigDecimal.random

--- lib/propr/monad/random.rb
+++ lib/propr/monad/random.rb
@@ -9,12 +9,16 @@ module Propr
     # Evaluators
     #############################################
 
-    def run(computation, scale = 1.0)
+    def run(computation, scale = BigDecimal(1))
       computation.call(scale)
     end
 
-    def eval(computation, scale = 1.0)
-      computation.call(scale)[0]
+    def eval(computation, scale = BigDecimal(1), retries = 0)
+      while true
+        value, scale, success = computation.call(scale)
+        return value if success
+        raise NoMoreTries, "unknown" if (retries -= 1) < 0
+      end
     end
 
     # Combinators
@@ -22,14 +26,17 @@ module Propr
 
     def unit(value)
       lambda do |scale|
-        [value, scale]
+        [value, scale, true]
       end
     end
 
     def bind(f, &g)
       lambda do |scale|
-        value, scale = f.call(scale)
-        g.call(value).call(scale)
+        value, scale, success = f.call(scale)
+
+        success ?
+          g.call(value).call(scale) :
+          [value, scale, success]
       end
     end
 
@@ -38,29 +45,46 @@ module Propr
 
     def guard(condition)
       lambda do |scale|
-        # [value, scale]
+        [nil, scale, condition]
       end
     end
 
-    def scale(number, zero = nil)
+    # When given zero arguments, returns the current scale factor
+    #
+    # When given one argument, sets the current scale factor to
+    # the given value, bounded between 0..1.
+    #
+    # When given two arguments, scales a numeric value towards a
+    # given origin `zero`, using the current scale factor (0..1).
+    #
+    def scale(number = nil, zero = nil)
       if zero.nil?
-        # Set scale factor 0..1
-        lambda do |scale|
-          scale = 1.0 if scale > 1.0
-          scale = 0.0 if scale < 0.0
-          [nil, scale.to_f]
+        if number.nil?
+          lambda do |scale|
+            [scale, scale, true]
+          end
+        else
+          # Set scale factor 0..1
+          lambda do |scale|
+            scale = scale.coerce(1)[0] if scale > 1
+            scale = scale.coerce(0)[0] if scale < 0
+            [nil, scale, true]
+          end
         end
       else
         # Scale given number towards zero
         lambda do |scale|
-          [zero + scale * (number - zero), scale]
+          [zero + scale * (number - zero), scale, true]
         end
       end
     end
 
+    # Generate psuedo-random number normally distributed between
+    # 0 <= x < 1. This distribution is not weighted using `scale`.
+    #
     def rand(limit = nil)
       lambda do |scale|
-        [Kernel.rand(limit), scale]
+        [Kernel.rand(limit), scale, true]
       end
     end
 
--- lib/propr/types/bigdecimal.rb
+++ lib/propr/types/bigdecimal.rb
@@ -15,19 +15,40 @@ class << BigDecimal
   NAN = BigDecimal('NaN')
 
   # @return [BigDecimal]
-  def random(options = {})
-    min = (options[:min] || -INF).to_f
-    max = (options[:max] ||  INF).to_f
+  def random(options = {}, m = Propr::Random)
+    min = BigDecimal(options[:min] || -INF)
+    max = BigDecimal(options[:max] ||  INF)
 
-    min_ = if min.finite? then min else -Float::MAX end
-    max_ = if max.finite? then max else  Float::MAX end
-
-    a = rand(max_.to_i - min_.to_i + 1)
-    b = rand(max_.to_i - min_.to_i + 1)
-
-    c  = BigDecimal(a.to_s) + BigDecimal(min_.to_s)
-    c += c/BigDecimal(b.to_s)
+    min_ = if min.finite? then min else BigDecimal(-Float::MAX.to_s) end
+    max_ = if max.finite? then max else BigDecimal( Float::MAX.to_s) end
 
     # @todo: -INF, +INF, -0.0, NAN
+    center = options.fetch(:center, :mid)
+    center =
+      case center
+      when :mid then min_ + (max_ - min_).div(2)
+      when :min then min
+      when :max then max
+      when Numeric
+        raise ArgumentError,
+          "center < min" if center < min
+        raise ArgumentError,
+          "center > max" if center > max
+        center
+      else raise ArgumentError,
+        "center must be :min, :mid, :max, or min <= Integer <= max"
+      end
+
+    # @todo: -INF, +INF, -0.0, NAN
+
+    m.bind(m.rand(max_ - min_)) do |a|
+      m.bind(m.rand(max_ - min_)) do |b|
+        c  = BigDecimal(a) + BigDecimal(min_.to_s)
+        c += c / BigDecimal(b) # not 0..1
+        c  = max if c > max
+        c  = min if c < min
+        m.scale(c, center)
+      end
+    end
   end
 end
