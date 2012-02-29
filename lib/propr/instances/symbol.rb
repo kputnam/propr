d6f50dfe52d53916f5e645b9313f41e690e9ea7c
kputnam <235798+kputnam@users.noreply.github.com>
2012-02-29 00:20:10 -0600

    Implement #shrink on Hash, Symbol, Time

---
+++ lib/propr/instances/symbol.rb
@@ -0,0 +1,19 @@
+class Symbol
+  def shrink
+    to_s.shrink.map(&:to_sym)
+  end
+end
+
+class << Symbol
+  # @note: Beware of memory consumption. Symbols are never garbage
+  # collected, and we're generating them at random!
+  #
+  def random(options = {}, m = Propr::Random)
+    min = options[:min] || 0
+    max = options[:max] || 10
+    options = Hash[charset: /[a-z_]/]
+    m.bind(String.random(options)) do |s|
+      m.unit(s.to_sym)
+    end
+  end
+end
