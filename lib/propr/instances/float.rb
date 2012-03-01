598bd24a6981dfef6ba33e1a7183dde3b193549f
kputnam <235798+kputnam@users.noreply.github.com>
2012-03-01 16:49:47 -0600

    Paint-over Float errors in intermediate calculations by using BigDecimal

--- lib/propr/instances/float.rb
+++ lib/propr/instances/float.rb
@@ -21,7 +21,7 @@ class << Float
     min_ = if min.finite? then min else -Float::MAX end
     max_ = if max.finite? then max else  Float::MAX end
 
-    range  = max_ - min_
+    range  = max_.to_i - min_.to_i
     center = options.fetch(:center, :mid)
     center =
       case center
@@ -45,10 +45,27 @@ class << Float
 
     # @todo: -Float::INFINITY, +Float::INFINITY, -0.0, Float::NAN
 
+    # One approach is to count all `n` possible Float values inside
+    # the [min, max] interval (n <= 2^64 for double precision), then
+    # generate a Fixnum inside [0, n] and map it back to the nth
+    # Float value in [min, max].
+    #
+    # Instead, this method just counts the `n` possible whole values
+    # inside [min, max], generates a Fixnum inside [0, n - 1], then
+    # maps that back to the nth whole value inside [min, max - 1].
+    # Next we tack on a random fractional value inside [0, 1).
     m.bind(m.rand(range)) do |whole|
       m.bind(m.rand) do |fraction|
-        value = BigDecimal(min_.to_s) + whole + fraction
-        m.scale(value, range, center)
+        # Need to perform scaling on BigDecimal to prevent floating
+        # point underflow and overflow (e.g., BIG / HUGE == 0.0, and
+        # BIG + small == BIG).
+        value  = BigDecimal(min_, 15) + whole
+        value += BigDecimal(fraction, 0)
+        center = BigDecimal(center, 0)
+
+        m.bind(m.scale(value, range, center)) do |d|
+          m.unit(d.to_f)
+        end
       end
     end
   end
