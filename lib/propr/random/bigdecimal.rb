36880b84a1ad0af1ce80a5f04396bd8897bf4a2a
kputnam <235798+kputnam@users.noreply.github.com>
2012-04-14 08:14:19 -0500

    Optimize shrink by generating /limited/ number of values

--- lib/propr/random/bigdecimal.rb
+++ lib/propr/random/bigdecimal.rb
@@ -3,10 +3,15 @@ require "bigdecimal"
 class BigDecimal
   # @return [Array<BigDecimal>]
   def shrink
+    limit = 10
+
     Array.unfold(self) do |seed|
-      zero  = 0
-      seed_ = zero + (seed - zero) / 2
-      ((seed - seed_).abs > 1e-10).maybe([self + zero - seed, seed_])
+      limit -= 1
+      zero   = 0
+      seed_  = zero + (seed - zero) / 2
+
+      (limit > 0 && (seed - seed_).abs > 1e-5)
+        .maybe([self + zero - seed, seed_])
     end
   end
 end
