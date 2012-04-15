a3f40b2d6ec416a19c2aef1a3a9b06e47b206069
kputnam <putnam.kyle@gmail.com>
2012-04-15 10:57:59 -0500

    Minor optimizations (safe by referential transparency)

--- lib/propr/instances/set.rb
+++ lib/propr/instances/set.rb
@@ -27,10 +27,11 @@ class << Set
   def random(options = {}, m = Propr::Random)
     min  = options[:min] || 0
     max  = options[:max] || 10
+    item = yield
 
     # @todo: Be sure we created enough *unique* elements
     m.bind(Integer.random(options.merge(min: min, max: max))) do |size|
-      m.bind(m.sequence(size.times.map { yield })) do |xs|
+      m.bind(m.sequence([item]*size)) do |xs|
         m.unit(xs.to_set)
       end
     end
