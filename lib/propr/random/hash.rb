13a842f44ea25dc9a43e146522e59054b3a53027
kputnam <235798+kputnam@users.noreply.github.com>
2012-06-19 16:51:58 -0500

    Create Hash.random_vals generator

--- lib/propr/random/hash.rb
+++ lib/propr/random/hash.rb
@@ -13,6 +13,11 @@ class << Hash
   #     m.bind(Integer.random){|k| m.bind(String.random){|v| m.unit([k,v]) }}
   #   end
   #
+  # @example
+  #   Hash.random do
+  #     m.sequence([Integer.random, String.random])
+  #   end
+  #
   def random(options = {}, m = Propr::Random)
     min  = options[:min] || 0
     max  = options[:max] || 10
@@ -29,6 +34,22 @@ class << Hash
       m.bind(m.sequence([pair]*size)) do |pairs|
         m.unit(Hash[pairs])
       end
+    end
+  end
+
+  # @example
+  #   Hash.random_vals \
+  #     name:   String.random,
+  #     count:  Integer.random(min: 0),
+  #     weight: Float.random
+  #
+  def random_vals(hash, m = Propr::Random)
+    # Convert hash of key => generator to a list of pair-generators,
+    # where the pairs correspond to the original set of [key, value]
+    pairs = hash.map{|k,v| m.bind(v){|v| m.unit([k, v]) }}
+
+    m.bind(m.sequence(pairs)) do |pairs|
+      m.unit(Hash[pairs])
     end
   end
 end
